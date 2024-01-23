d8e7117666f68c7a2c6a14ae1cde27f722011723
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-23 12:47:00 -0400

    ESlint fix.

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,9 +4,10 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 
-request(url, function (err, response, body) {
-    if (err) {
-      console.log(err);
-    }
+request(url, (err, res, body) => {
+   if (err) {
+    console.log(err);
+   } else {
     console.log(JSON.parse(body).title);
-  });
+   }
+});
