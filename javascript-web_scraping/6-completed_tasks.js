56e96656c82ad5a124be24c2328286864cf3b4a1
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-24 17:58:43 -0400

    Semistandard fix.

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,20 +4,20 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (err, res, body) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    const completedTasks = {};
-    const tasks = JSON.parse(body);
-    for (const task of tasks) {
-        if (task.completed === true) {
-            if (completedTasks[task.userId] === undefined) {
-                completedTasks[task.userId] = 1;
-            } else {
-                completedTasks[task.userId] += 1;
-            }
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const completedTasks = {};
+  const tasks = JSON.parse(body);
+  for (const task of tasks) {
+    if (task.completed === true) {
+        if (completedTasks[task.userId] === undefined) {
+            completedTasks[task.userId] = 1;
+        } else {
+            completedTasks[task.userId] += 1;
         }
     }
-    console.log(completedTasks);
+}
+  console.log(completedTasks);
 });
