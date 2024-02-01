5187e0086a570b06445db0ab8743a0f98f585417
GreyLeaf23 <christian.torres2894@gmail.com>
2024-02-01 16:27:15 -0400

    Program interacts with user, asking their name and displays it.

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('Welcome to Holberon School, what is your name?');
+
+rl.on('line', (input) => {
+    console.log(`Your name is: ${input}`);
+    rl.close();
+});
+
+rl.on('close', () => {
+    console.log('This important software is now closing');
+});
