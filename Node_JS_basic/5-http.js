b777b205f259d452f40a722e822462aaa6bdb43d
GreyLeaf23 <christian.torres2894@gmail.com>
2024-02-02 18:28:32 -0400

    Test drive.

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,29 +2,26 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const hostname = '127.0.0.1';
-const port = 1245;
+const DATABASE = 'database.csv';
 
-const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    let dbInfo = 'This is the list of our students\n';
-    await countStudents(process.argv[2])
-      .then((msg) => {
-        dbInfo += msg;
-        res.end(dbInfo);
+    countStudents(DATABASE)
+      .then((data) => {
+        res.end(`This is the list of our students\n${data}`);
       })
       .catch((err) => {
-        dbInfo += err.message;
-        res.end(dbInfo);
+        res.end(err.message);
       });
+  } else {
+    res.statusCode = 404;
+    res.end('Not found');
   }
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`);
-});
+app.listen(1245);
 
 module.exports = app;
