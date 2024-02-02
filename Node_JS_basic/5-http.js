61afcde5e7b1a0dff78bca6c7c21356200eafce4
GreyLeaf23 <christian.torres2894@gmail.com>
2024-02-02 18:30:57 -0400

    Test drive.

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,26 +2,20 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const DATABASE = 'database.csv';
-
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    countStudents(DATABASE)
-      .then((data) => {
-        res.end(`This is the list of our students\n${data}`);
-      })
-      .catch((err) => {
-        res.end(err.message);
-      });
-  } else {
-    res.statusCode = 404;
-    res.end('Not found');
+  if (req.url === '/') res.write('Hello Holberton School!');
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const data = await countStudents(process.argv[2]);
+      res.end(`${data.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
   }
+  res.end();
 });
-
 app.listen(1245);
-
 module.exports = app;
